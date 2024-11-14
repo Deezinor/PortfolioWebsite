48320c8e06564cb05c0aeccaedd35c98e0c68044
Jake Boulton <jboultonwork@gmail.com>
2024-11-14 14:16:15 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './inbex.css'
+import Footer from './components/footer'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <body>
+      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+
+      </body>
+      <Footer />
+    </>
+  )
+}
+
+export default App
